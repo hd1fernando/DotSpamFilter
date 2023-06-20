2247009bce96b972e62e38e1b78d473aa914e516
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2023-06-20 20:49:27 -0300

    feat: create trainig structure

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,50 @@
-﻿var csvPath = "spam.csv";
+﻿var NormalWords = new Dictionary<string, WordInfo>();
+var SpamlWords = new Dictionary<string, WordInfo>();
+
+int totalSpanWords = 0;
+int totalNormalWords = 0;
+
+var csvPath = "spam.csv";
 
 var lines = File.ReadAllLines(csvPath);
-int linha = 1;
+int maxLine = (int)Math.Floor(lines.Length * 0.75);
+int validationLineAfter = maxLine + 1;
 
 foreach (var line in lines)
 {
     var splitedLine = line.Split('|', StringSplitOptions.RemoveEmptyEntries);
     var flag = splitedLine[0];
     var message = splitedLine[1];
+
+    message = RemovePonctuations(message);
+    message = RemoveStopWords(message);
+    AddWordsToDictionaries(message, flag);
+
     Console.WriteLine(flag + " " + message);
-    linha++;
+
+    maxLine--;
+    if (maxLine <= 0)
+        break;
+}
+
+void AddWordsToDictionaries(string message, string flag)
+{
+    throw new NotImplementedException();
+}
+
+string RemoveStopWords(string message)
+{
+    throw new NotImplementedException();
+}
+
+string RemovePonctuations(string message)
+{
+    throw new NotImplementedException();
+
+}
+
+public class WordInfo
+{
+    public required int Quantity { get; set; }
+    public double Probability { get; set; }
 }

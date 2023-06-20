f2fd7d5587fb7d46b2a528db99ad035b6336a557
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2023-06-20 20:37:28 -0300

    feat: add csv file

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var csvPath = "spam.csv";
+
+var lines = File.ReadAllLines(csvPath);
+int linha = 1;
+
+foreach (var line in lines)
+{
+    var splitedLine = line.Split('|', StringSplitOptions.RemoveEmptyEntries);
+    var flag = splitedLine[0];
+    var message = splitedLine[1];
+    Console.WriteLine(flag + " " + message);
+    linha++;
+}
